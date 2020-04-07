59ac271509b11caa9efa3f83d8e583b5e4a212bf
Kishan <kishankrs@outlook.com>
2020-04-07 21:05:26 +0530

    Updates

---
+++ precache-manifest.d2ce0a1bc32b8372634b58f1cf70ab0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25451e05f438760c0ddd42931a4b14d0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2e72065f6b1227a44375",
+    "url": "/monsters-rolodex/static/css/main.e70118e2.chunk.css"
+  },
+  {
+    "revision": "bdefe47a8cca91deeb59",
+    "url": "/monsters-rolodex/static/js/2.a7a88c6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a7a88c6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e72065f6b1227a44375",
+    "url": "/monsters-rolodex/static/js/main.7497e4df.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  }
+]);
